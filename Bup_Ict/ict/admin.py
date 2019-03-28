d4243e355a9fdb57d4fa9bfb91cda7e4372ab53a
Revel109 <revelyusuf109@gmail.com>
2019-03-28 22:14:58 +0600

    bup-ict site in django

---
+++ Bup_Ict/ict/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Events, Documents, Admission, Curriculum, Faculty, Staff, ResearchGroup, Notice, HonorBoard, Alumni
+
+admin.site.register(Events)
+admin.site.register(Documents)
+admin.site.register(Admission)
+admin.site.register(Curriculum)
+admin.site.register(Faculty)
+admin.site.register(Staff)
+admin.site.register(ResearchGroup)
+admin.site.register(Notice)
+admin.site.register(HonorBoard)
+admin.site.register(Alumni)
